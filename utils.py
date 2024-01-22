5534637af2ed25b5a89bb5380f7598c25c14b37c
Luca Freckmann <luca.freckmann@uni-goettingen.de>
2024-01-22 11:04:07 +0100

    added 'image' to all images and 'labels' to all label-images

--- utils.py
+++ utils.py
@@ -71,7 +71,7 @@ def convert_hdf5_to_tif(paths, file_folders, data_folder):
         table_infected = f["tables/infected_labels/cells"][:]
         #print(f'Image Dataset info: Shape={marker.shape},Dtype={marker.dtype}')
         img_ds = {
-            "marker": marker, 
+            "marker_image": marker, 
             "nucleus_image": nucleus_image, 
             "cell_labels": cells, 
             "infected_labels": infected_labels,
